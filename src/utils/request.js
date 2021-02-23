830429a197c7355f97677da94c18c41a723bb5d1
张翼 <2210841826@qq.com>
2021-02-23 16:51:25 +0800

    项目初始化

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 封装axios请求模块
 const request = axios.create({
-    baseURL:'http://ttapi.research.itcast.cn/' //基础路径
-}) 
+  baseURL: 'http://ttapi.research.itcast.cn/'// 基础路径
+})
 
 export default request
